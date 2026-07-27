0dd8b4579a868b6858df6e7baf29161ad7f2f18e
JDK <luciani29102003@gmail.com>
2026-07-27 17:58:51 -0500

    agen

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -171,7 +171,7 @@ export default function Agenda() {
   }
 
   return (
-    <div className="flex flex-col h-full p-6 max-w-7xl mx-auto">
+    <div className="flex flex-col h-full p-6">
       {error && (
         <div className="fixed top-4 right-4 bg-red-50 border border-red-200 text-red-700 text-sm px-4 py-2 rounded-lg z-50">
           {error}
